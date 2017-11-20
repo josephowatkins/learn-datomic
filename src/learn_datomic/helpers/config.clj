4a8fbb2c657b364c06b1cc655f6ac62881812750
Joseph Watkins <joseph.o.watkins@gmail.com>
2017-11-20 23:04:28 +0000

    Use environ for config

--- src/learn_datomic/helpers/config.clj
+++ src/learn_datomic/helpers/config.clj
@@ -1,3 +1,5 @@
-(ns learn-datomic.helpers.config)
+(ns learn-datomic.helpers.config
+  (:require [environ.core :refer [env]]))
 
-(def datomic-uri "datomic:free://localhost:4334/films")
+(def datomic-uri
+  (env :datomic-uri "datomic:free://localhost:4334/films"))
